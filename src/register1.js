f393b7e1281f762f823bc63d6da92e7087f5d57a
wallermatt <waller.matthew.alt@gmail.com>
2020-01-13 21:58:13 +0000

    src

---
+++ src/register1.js
@@ -0,0 +1,84 @@
+import web3 from './web3';
+
+const address = '0x71ed3E7fCa6eAF3f1f3cce1d57FEe066d38c8682';
+
+const abi = [
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "records",
+		"outputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "information",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "recordAddress",
+				"type": "address"
+			}
+		],
+		"name": "getRecord",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "information",
+				"type": "string"
+			}
+		],
+		"name": "createRecord",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+]
+
+export default new web3.eth.Contract(abi, address);
